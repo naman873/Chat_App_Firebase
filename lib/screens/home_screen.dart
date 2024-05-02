f05e390a36fa4b9138fd19393201c24a68a42bba
namandhiman <namandhiman98@gmail.com>
2024-05-02 09:56:35 +0530

    changes..

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,7 +14,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Users"),
+        title: const Text("USERS"),
         automaticallyImplyLeading: false,
         centerTitle: true,
         actions: [
